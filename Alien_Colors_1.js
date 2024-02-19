53216a6faea9e0a7dcce94f697e2ca57ddb1cb9f
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:14:44 -0800

    Add files via upload

---
+++ Alien_Colors_1.js
@@ -0,0 +1,14 @@
+"use strict";
+// written by : TAYYABA RAO
+// Date : 18 / feb/ 2024
+// Alien's  color define 
+let alien_color = 'green';
+{
+    console.log("Players just earend 5 Rpees!");
+}
+//Alien's color change 
+alien_color = 'yellow';
+//this code couden't run bcz condition is false
+if (alien_color === 'green') {
+    console.log("Players just earned 5 Rupee!");
+}
